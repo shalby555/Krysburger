6c0a46d219c5c0566e626aad414fa20e8877f577
Sergye_Naughty <shalby555@gmail.com>
2024-10-15 16:39:21 +0300

    add OutputModel

---
+++ Krysburger.Core/OutputModels/LeftoverFoodOutputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Krysburger.Core.OutputModels
+{
+    internal class LeftoverFoodOutputModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+    }
+}
