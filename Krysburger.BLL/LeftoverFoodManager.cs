7ac9e19e1ec646f209e93a14fc3f6ea3b3204e28
Sergye_Naughty <shalby555@gmail.com>
2024-10-15 19:20:15 +0300

    Add LeftoverFood BLL & Razor & ProductCard

---
+++ Krysburger.BLL/LeftoverFoodManager.cs
@@ -0,0 +1,34 @@
+﻿using Krysburger.Core.OutputModels;
+namespace Krysburger.BLL
+{
+    public class LeftoverFoodManager
+    {
+        public List<LeftoverFoodOutputModel> GetAllLeftoverFood()
+        {
+            return new List<LeftoverFoodOutputModel>
+            {
+                new LeftoverFoodOutputModel ()
+                {
+                    Id=1,
+                    Name="Egs",
+                    Count=150
+                },
+
+                new LeftoverFoodOutputModel ()
+                {
+                    Id=2,
+                    Name="Meat",
+                    Count=300
+                },
+
+                new LeftoverFoodOutputModel()
+                {
+                    Id=3,
+                    Name="Chicken",
+                    Count=200
+                },
+            };
+        }
+    }
+}
+
