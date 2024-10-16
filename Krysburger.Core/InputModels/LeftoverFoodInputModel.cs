b2bbae0ca073ccfd9e622e87f23f5e1943f90edb
Sergye_Naughty <shalby555@gmail.com>
2024-10-16 20:48:13 +0300

    Add Input Model

---
+++ Krysburger.Core/InputModels/LeftoverFoodInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Krysburger.Core.InputModels
+{
+    public class LeftoverFoodInputModel
+    {
+        public string Name { get; set; }
+        public string Measure { get; set; }
+        public int Count { get; set;}
+    }
+}
