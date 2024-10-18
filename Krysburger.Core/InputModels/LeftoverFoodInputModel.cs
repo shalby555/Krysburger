f85e2ec4e1f63e44b70cbff7fa42e859f8d7654c
Sergye_Naughty <shalby555@gmail.com>
2024-10-18 17:02:53 +0300

    add component

--- Krysburger.Core/InputModels/LeftoverFoodInputModel.cs
+++ Krysburger.Core/InputModels/LeftoverFoodInputModel.cs
@@ -4,8 +4,13 @@ namespace Krysburger.Core.InputModels
 {
     public class LeftoverFoodInputModel
     {
+        [Required(ErrorMessage = "Пожалуйста,укажите название продукта")]   /*правило заполнение формы и сообщение об ошибке*/
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Быстрее)))")]
         public string Name { get; set; }
         public string Measure { get; set; }
-        public int Count { get; set;}
+
+        [Required(ErrorMessage = "Пожалуйста,укажите кол-во продукта")]
+        [Range(1, int.MaxValue)]
+        public int? Count { get; set;}
     }
 }
